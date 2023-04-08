99aed332b4983f5a6cfb86c1ebe5b6208f186d00
Victor Feng He <victorfeng2000@gmail.com>
2023-04-08 15:01:14 -0400

    Added Mode Select Screen and added the necessary buttons to the layout.

---
+++ StarQuiz/app/src/main/java/com/example/starquiz/ModeSelect.java
@@ -0,0 +1,14 @@
+package com.example.starquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModeSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mode_select);
+    }
+}
