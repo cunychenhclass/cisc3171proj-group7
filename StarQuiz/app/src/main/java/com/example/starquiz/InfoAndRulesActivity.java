ececf0b61bf3750b5c6da773c7e21e3ee8ce97b9
Max Yedid <maxyedid1@gmail.com>
2023-04-08 23:57:09 -0400

    Added functionality to prevent user from moving forward if they haven't entered their username.

--- StarQuiz/app/src/main/java/com/example/starquiz/InfoAndRulesActivity.java
+++ StarQuiz/app/src/main/java/com/example/starquiz/InfoAndRulesActivity.java
@@ -2,6 +2,8 @@ package com.example.starquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.icu.text.IDNA;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +14,8 @@ public class InfoAndRulesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_and_rules);
+
+        Bundle extras = getIntent().getExtras();
 
         Button returnButton = (Button)findViewById(R.id.returnButton);
 
@@ -24,10 +28,16 @@ public class InfoAndRulesActivity extends AppCompatActivity {
 
         Button okButton = (Button)findViewById(R.id.okButton);
 
+        // To prevent the user from getting into the quiz without logging in
+        if (extras == null || !extras.containsKey("username")) {
+            okButton.setVisibility(View.INVISIBLE);
+        }
+
         okButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                Intent intent = new Intent(InfoAndRulesActivity.this, ModeSelect.class);
+                startActivity(intent);
             }
         });
     }
