3f3c0965ac7e2b8eafc759148d903b60a6e9ac1f
Kamil-S85 <kamil.skorupa85@gmail.com>
2023-04-08 20:02:00 -0400

    Home Activity
    Home Activity for the Home screen. Has the text field for the app name. Textbox to enter username and check against banned username DB (implementation TBD), if valid, upon pressing the Enter button, user proceeds to Info & Rules screen. 3 buttons below will be: Leaderboards, which proceeds to the Leaderboard Select screen, Info & Rules, and Themes.

---
+++ StarQuiz/app/src/main/java/com/example/starquiz/HomeActivity.java
@@ -0,0 +1,98 @@
+// Import necessary classes and packages
+package com.example.starquiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// MainActivity base class for activities
+public class MainActivity extends AppCompatActivity {
+
+    // Private variables for EditText, username, and bannedUsernames
+    private EditText usernameEditText;
+    private String username;
+    private Set<String> bannedUsernames;
+
+    // onCreate method called when activity created
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find views by their ID in layout and assign them to variables
+        TextView appName = findViewById(R.id.appName);
+        usernameEditText = findViewById(R.id.usernameEditText);
+        Button enterButton = findViewById(R.id.enterButton);
+        Button leaderboardsButton = findViewById(R.id.leaderboardsButton);
+        Button infoRulesButton = findViewById(R.id.infoRulesButton);
+        Button themesButton = findViewById(R.id.themesButton);
+
+        // Initialize bannedUsernames set
+        bannedUsernames = new HashSet<>();
+        // bannedUsername DB implementation here
+
+        // Set onClickListener for enterButton to handle user clicks
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get entered username from EditText and trim any whitespace
+                username = usernameEditText.getText().toString().trim();
+
+                // Check if entered username is valid
+                if (isValidUsername(username)) {
+                    // If username is valid create an intent to navigate to the InfoAndRulesActivity
+                    Intent intent = new Intent(HomeActivity.this, InfoAndRulesActivity.class);
+                    // Pass username to next activity using intent.putExtra
+                    intent.putExtra("username", username);
+                    // Start InfoAndRulesActivity
+                    startActivity(intent);
+                } else {
+                    // If username is invalid show a toast message informing the user
+                    Toast.makeText(HomeActivity.this, "Invalid username. Please choose another.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Set onClickListener for leaderboardsButton to navigate to LeaderboardSelectActivity
+        leaderboardsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, LeaderboardSelectActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set onClickListener for infoRulesButton to navigate to InfoAndRulesActivity
+        infoRulesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, InfoAndRulesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set onClickListener for themesButton to navigate to ThemesActivity
+        themesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, ThemesActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    // Private helper method to check if a username is valid
+    private boolean isValidUsername(String username) {
+        // Return true if username is not empty and is not in bannedUsernames database. Implementation to be determined.
+        return !username.isEmpty() && !bannedUsernames.contains(username);
+    }
+}
