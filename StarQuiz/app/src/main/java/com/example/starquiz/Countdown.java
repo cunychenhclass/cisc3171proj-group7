ea9e3cf2f7d946803c827537b1ae75cc141c8142
Max Yedid <maxyedid1@gmail.com>
2023-04-10 02:09:05 -0400

    Refactored some of the code for the animation to look a bit cleaner and efficient. Also added an alternative way of doing the animation which is currently commented out but available.

--- StarQuiz/app/src/main/java/com/example/starquiz/Countdown.java
+++ StarQuiz/app/src/main/java/com/example/starquiz/Countdown.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
@@ -19,24 +20,29 @@ import java.util.logging.Logger;
 
 public class Countdown extends AppCompatActivity {
 
+    TextView threeText;
+    TextView twoText;
+    TextView oneText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_countdown);
 
-        TextView threeText = findViewById(R.id.threeCountdown);
-        TextView twoText = findViewById(R.id.twoCountdown);
-        TextView oneText = findViewById(R.id.oneCountdown);
+        threeText = findViewById(R.id.threeCountdown);
+        twoText = findViewById(R.id.twoCountdown);
+        oneText = findViewById(R.id.oneCountdown);
 
-        fadeInAnimation(threeText, twoText, oneText);
+        fadeInAnimation(threeText);
 
     }
 
+
     //TODO, Refactor all this code as it does work but it looks messy
 
-    public void fadeOutAnimation(TextView text, TextView nextText, TextView lastText) {
+    public void fadeOutAnimation(TextView text) {
         Animation out = new AlphaAnimation(1.0f, 0.0f);
-        out.setDuration(1000);
+        out.setDuration(750);
 
         out.setAnimationListener(new Animation.AnimationListener() {
             @Override
@@ -47,12 +53,13 @@ public class Countdown extends AppCompatActivity {
             @Override
             public void onAnimationEnd(Animation animation) {
                 text.setAlpha(0.0f);
-                if (nextText != null) {
-                    fadeInAnimation(nextText, lastText, null);
+                int currCount = Integer.parseInt(text.getText().toString());
+                if (currCount == 3) {
+                    fadeInAnimation(twoText);
+                } else if (currCount == 2) {
+                    fadeInAnimation(oneText);
                 } else {
-                    Log.i("going to new screen","we are done with counting");
-                    //Intent intent = new Intent(Countdown.this, Quiz.class);
-                    //startActivity(intent);
+                    Log.i("Countdown", "Should move to next screen here");
                 }
             }
 
@@ -64,10 +71,10 @@ public class Countdown extends AppCompatActivity {
         text.startAnimation(out);
     }
 
-    public void fadeInAnimation(TextView text, TextView nextText, TextView lastText) {
+    public void fadeInAnimation(TextView text) {
         text.setAlpha(1.0f);
         Animation in = new AlphaAnimation(0.0f, 1.0f);
-        in.setDuration(1000);
+        in.setDuration(750);
 
         in.setAnimationListener(new Animation.AnimationListener() {
             @Override
@@ -77,12 +84,7 @@ public class Countdown extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                text.setAlpha(1.0f);
-                if (nextText != null) {
-                    fadeOutAnimation(text, nextText, lastText);
-                } else {
-                    fadeOutAnimation(text, null, null);
-                }
+                fadeOutAnimation(text);
             }
 
             @Override
@@ -93,4 +95,60 @@ public class Countdown extends AppCompatActivity {
         });
         text.startAnimation(in);
     }
+
+    // This is an alternative way to doing the animation
+        /*public void fadeInAnimation(TextView text) {
+        text.setAlpha(1.0f);
+        Animation in = new AlphaAnimation(0.0f, 1.0f);
+        in.setDuration(750);
+
+        in.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                fadeOutAnimation(text);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        text.startAnimation(in);
+    }
+
+    public void fadeOutAnimation(TextView text) {
+        Animation out = new AlphaAnimation(1.0f, 0.0f);
+        out.setDuration(750);
+
+        out.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                text.setAlpha(0.0f);
+                int currentCount = Integer.parseInt(text.getText().toString());
+                if (currentCount > 1) {
+                    currentCount--;
+                    text.setText(currentCount + "");
+                    fadeInAnimation(text);
+                } else {
+                    Log.i("Countdown", "moving to new screen");
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        text.startAnimation(out);
+    }*/
 }
