2b2125a2bdbeafa9f6725a0c1350bfb62e7eb5bf
jaden Guevara <officialjadenguevara@gmail.com>
2023-04-19 19:59:38 -0400

    set up lifeline button commands + linkage leaderboard

--- StarQuiz/app/src/main/java/com/example/starquiz/LeaderboardSelect.java
+++ StarQuiz/app/src/main/java/com/example/starquiz/LeaderboardSelect.java
@@ -11,23 +11,27 @@ import android.widget.ImageButton;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Random;
 
 public class LeaderboardSelect extends AppCompatActivity {
 
-//    ArrayList<LeaderboardArrayList> cate10 = new ArrayList<>();
-//    ArrayList<LeaderboardArrayList> cate20 = new ArrayList<>();
-//    ArrayList<LeaderboardArrayList> cate30 = new ArrayList<>();
-//    ArrayList<LeaderboardArrayList> cate40 = new ArrayList<>();
-//    ArrayList<LeaderboardArrayList> cate50 = new ArrayList<>();
-//    ArrayList<LeaderboardArrayList> cate100 = new ArrayList<>();
+    LeaderboardArrayList cate10;
+    LeaderboardArrayList cate20;
+    LeaderboardArrayList cate30;
+    LeaderboardArrayList cate40;
+    LeaderboardArrayList cate50;
+    LeaderboardArrayList cate100;
 
     ArrayList<Button> buttonsList = new ArrayList<>();
-    int categoryusing;
+    LeaderboardArrayList categoryusing;
+    int categoryNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard_select);
+
+
 
         Button leaderboard10 = (Button)findViewById(R.id.button10Leaderboard);
         Button leaderboard20 = (Button)findViewById(R.id.button20Leaderboard);
@@ -43,7 +47,6 @@ public class LeaderboardSelect extends AppCompatActivity {
         buttonsList.add(leaderboard50);
         buttonsList.add(leaderboard100);
 
-
         ImageButton returnButton = (ImageButton)findViewById(R.id.returnButtonLeaderboard);
         Button selectButton = (Button)findViewById(R.id.nextLeaderboardSelectButton);
         returnButton.setOnClickListener(new View.OnClickListener() {
@@ -56,48 +59,54 @@ public class LeaderboardSelect extends AppCompatActivity {
         leaderboard10.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                categoryusing = 10;
+                categoryusing = cate10;
                 colorChange(leaderboard10);
+                categoryNum = 10;
             }
         });
 
         leaderboard20.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                categoryusing = 20;
+                categoryusing = cate20;
                 colorChange(leaderboard20);
+                categoryNum = 20;
             }
         });
 
         leaderboard30.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                categoryusing = 30;
+                categoryusing = cate30;
                 colorChange(leaderboard30);
+                categoryNum = 30;
             }
         });
 
         leaderboard40.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                categoryusing = 40;
+                categoryusing = cate40;
                 colorChange(leaderboard40);
+                categoryNum = 40;
             }
         });
 
         leaderboard50.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                categoryusing = 50;
+                categoryusing = cate50;
                 colorChange(leaderboard50);
+                categoryNum = 50;
             }
         });
 
         leaderboard100.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                categoryusing = 100;
+                categoryusing = cate100;
                 colorChange(leaderboard100);
+                categoryNum = 100;
             }
         });
 
@@ -105,9 +114,14 @@ public class LeaderboardSelect extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //use category int
+                testStocks(categoryusing);
                 Intent intent = new Intent(LeaderboardSelect.this, SpecificLeaderboard.class);
-                intent.putExtra("LeaderboardCategory", categoryusing);
+                intent.putExtra("categoryNum", categoryNum);
+                intent.putStringArrayListExtra("NamesList", categoryusing.username);
+                intent.putIntegerArrayListExtra("ScoreList", categoryusing.score);
+                intent.putExtra("TimeList", categoryusing.time);
                 startActivity(intent);
+
             }
         });
 
@@ -122,12 +136,12 @@ public class LeaderboardSelect extends AppCompatActivity {
         category_.setBackgroundColor(getResources().getColor(R.color.purple_200));
     }
 
-//    public void submitUser(int category_, String Username_, int Score_, float time_)
+//    public void submitUser(LeaderboardArrayList category, String Username_, int Score_, float time_)
 //    {
 //        switch (category_)
 //        {
 //            case 10:
-//                arrayListHandler(cate10, Username_, Score_, time_);
+//
 //                break;
 //            case 20:
 //                arrayListHandler(cate20, Username_, Score_, time_);
@@ -147,35 +161,62 @@ public class LeaderboardSelect extends AppCompatActivity {
 //        }
 //    }
 
-//    public void arrayListHandler(ArrayList<LeaderboardArrayList> category, String Username_, int Score_, float time_)
-//    {
-//        for(int i = 0; i < category.size() - 1; i++)
-//        {
-//            if(category.get(i).username == Username_)
-//            {
-//                if(category.get(i).score < Score_)
-//                {category.get(i).score = Score_;}
-//            }
-//            else
-//            {
-//                category.add( new LeaderboardArrayList(Username_, Score_, time_));
-//            }
-//        }
-//    }
+    public void arrayListHandler(LeaderboardArrayList category, String Username_, int Score_, long time_)
+    {
+        for(int i = 0; i < category.username.size(); i++)
+        {
+            if(category.username.get(i) == Username_)
+            {
+                if(category.score.get(i) < Score_)
+                {
+                    category.score.set(i, Score_);
+                    category.time.set(i, time_);
+                }
+            }
+            else
+            {
+                category.username.add(Username_);
+                category.score.add(Score_);
+                category.time.add(time_);
+            }
+        }
+    }
+
+    public void testStocks(LeaderboardArrayList category)
+    {
+        String[] names = { "kyle", "jones", "James", "Tyler", "Kevin", "alex", "richie", "sasha", "beau"};
+        category.scrubArrays();
+        Random random = new Random();
+        for (int i = 0; i < names.length; i++)
+        {
+            arrayListHandler(category, names[i], random.nextInt(999), random.nextLong());
+        }
+
+    }
+
 
 }
 
-//class LeaderboardArrayList{ //used as a custom array type for the category lists
-//    int size = 10;
-//    String username;
-//    int score;
-//    float time;
+class LeaderboardArrayList{ //used as a custom array type for the category lists
+    int size = 10;
+    ArrayList<String> username = new ArrayList<>();
+    ArrayList<Integer> score = new ArrayList<>();
+    ArrayList<Long> time = new ArrayList<>();
+
+    //final float[] arr = new float[time.size()];
 //    LeaderboardArrayList( String Username_, int Score_, float time_)
 //    { //holds the individual datas to find the users
-//        this.username = Username_;
-//        this.score = Score_;
-//        this.time = time_;
+//        this.username.add(Username_);
+//        this.score.add(Score_);
+//        this.time.add(time_);
 //    }
-//
-//
-//}
+
+    public void scrubArrays()
+    {
+        username.clear();
+        score.clear();
+        time.clear();
+    }
+
+
+}
--- StarQuiz/app/src/main/java/com/example/starquiz/SpecificLeaderboard.java
+++ StarQuiz/app/src/main/java/com/example/starquiz/SpecificLeaderboard.java
@@ -2,6 +2,7 @@ package com.example.starquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,20 +10,38 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import org.w3c.dom.Text;
 
 public class SpecificLeaderboard extends AppCompatActivity {
+    ArrayList<String> username = new ArrayList<>();
+    ArrayList<Integer> score = new ArrayList<>();
+    ArrayList<Float> time = new ArrayList<>();
 
+    float[] unconvertedTime;
+    int categoryNum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_specific_leaderboard);
 
+        Bundle bundle = getIntent().getExtras();
+        categoryNum = getIntent().getIntExtra("categoryNum", categoryNum);
+        username = getIntent().getStringArrayListExtra("NamesList");
+        score = getIntent().getIntegerArrayListExtra("ScoreList");
+        unconvertedTime = getIntent().getFloatArrayExtra("TimeList");
+
+        for (float value: unconvertedTime) {
+            time.add(value);
+        }
+
         Button returnButton = findViewById(R.id.specificLeaderboardReturnButton);
+        TextView categoryName = findViewById(R.id.SpecificLeaderBoardSelectTitle);
+        categoryName.setText(categoryNum + " Questions Leaderboard");
 
         TableLayout mainLeaderboard = findViewById(R.id.specificLeaderboardTable);
-        for (int i = 1; i <= 100; i++) {
-            mainLeaderboard.addView(createNewRow(i, "Test User", 500, "00:05:30"));
+        for (int i = 1; i <= username.size(); i++) {
+            mainLeaderboard.addView(createNewRow(i, username.get(i), score.get(i), "00:05:00"));
         }
         returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
