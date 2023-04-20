035eb95c0f747bb65c3095ba97bcedfcc859dcd8
jaden Guevara <officialjadenguevara@gmail.com>
2023-04-19 23:41:13 -0400

    slight leaderboard refactoring on recieving data

--- StarQuiz/app/src/main/java/com/example/starquiz/SpecificLeaderboard.java
+++ StarQuiz/app/src/main/java/com/example/starquiz/SpecificLeaderboard.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
@@ -14,35 +15,48 @@ import java.util.ArrayList;
 import org.w3c.dom.Text;
 
 public class SpecificLeaderboard extends AppCompatActivity {
-    ArrayList<String> username = new ArrayList<>();
-    ArrayList<Integer> score = new ArrayList<>();
-    ArrayList<Float> time = new ArrayList<>();
+    String[] username;
+    int[] score;
+    float[] time;
+    String[] timeString;
 
-    float[] unconvertedTime;
     int categoryNum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_specific_leaderboard);
 
-        Bundle bundle = getIntent().getExtras();
-        categoryNum = getIntent().getIntExtra("categoryNum", categoryNum);
-        username = getIntent().getStringArrayListExtra("NamesList");
-        score = getIntent().getIntegerArrayListExtra("ScoreList");
-        unconvertedTime = getIntent().getFloatArrayExtra("TimeList");
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        categoryNum = bundle.getInt("categoryNum", categoryNum);
+        username = bundle.getStringArray("NamesList");
+        score = bundle.getIntArray("ScoreList");
+        time = bundle.getFloatArray("TimeList");
 
-        for (float value: unconvertedTime) {
-            time.add(value);
+        if(time != null)
+        {
+            for (int i = 0; i < time.length; i++) {
+                timeString[i] = Float.toString(time[i]);
+            }
         }
 
+
+        
         Button returnButton = findViewById(R.id.specificLeaderboardReturnButton);
-        TextView categoryName = findViewById(R.id.SpecificLeaderBoardSelectTitle);
+       TextView categoryName = findViewById(R.id.SpecificLeaderBoardSelectTitle);
         categoryName.setText(categoryNum + " Questions Leaderboard");
 
         TableLayout mainLeaderboard = findViewById(R.id.specificLeaderboardTable);
-        for (int i = 1; i <= username.size(); i++) {
-            mainLeaderboard.addView(createNewRow(i, username.get(i), score.get(i), "00:05:00"));
+        if(score != null)
+        {
+            for (int i = 0; i < username.length; i++) {
+                mainLeaderboard.addView(createNewRow(i + 1, username[i], score[i], "99:99:99"));
+            }
         }
+
+//        for (int i = 1; i <= 100; i++) {
+//            mainLeaderboard.addView(createNewRow(i, "Test User", 500, "00:05:30"));
+//        }
         returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
